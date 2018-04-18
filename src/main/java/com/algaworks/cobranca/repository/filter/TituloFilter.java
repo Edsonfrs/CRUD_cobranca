18412485c74b6bade9f43a8863ebdead0e4d6148
Edsonfrs <edsonfrs@gmail.com>
2018-04-18 11:22:53 -0300

    Envio demais arquivos

---
+++ src/main/java/com/algaworks/cobranca/repository/filter/TituloFilter.java
@@ -0,0 +1,18 @@
+package com.algaworks.cobranca.repository.filter;
+
+public class TituloFilter {
+	
+	private String descricao;
+
+	
+	public String getDescricao() {
+		// TODO Auto-generated method stub
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+
+}
